c643d46ad25672880481ba09f396e024ea9589b6
HoangTran0308 <tranthanhhoang29@gmail.com>
2025-04-15 22:43:36 +0700

    Add files via upload

---
+++ giftos-html/js/custom.js
@@ -0,0 +1,40 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// owl carousel 
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+// Hiển thị form khi vào trang
+window.onload = function() {
+    document.getElementById("registrationModal").style.display = "flex";
+};
+
+// Hàm đóng form
+function closeModal() {
+    document.getElementById("registrationModal").style.display = "none";
+}
+
